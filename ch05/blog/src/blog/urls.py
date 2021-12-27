ff9066221b7f8fd3ad0cf99ffc7c4fa5f840f48d
yc.liu <YungChiLiu@NCBM-PCP1911009.local>
2021-12-27 14:00:19 +0800

    [modify]Add individual page for post list

--- ch05/blog/src/blog/urls.py
+++ ch05/blog/src/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import BlogListView
+from .views import BlogDetailView, BlogListView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    # use pk(primary key) as int for path
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
 ]
