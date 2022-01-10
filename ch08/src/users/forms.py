971e169f91e58d287cb10d48c56d0139108ead7f
yc.liu <YungChiLiu@NCBM-PCP1911009.local>
2022-01-10 17:25:08 +0800

    init ch08 Custom User fields.

---
+++ ch08/src/users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import fields
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+class CustomUserChangeForm(UserChangeForm):
+    
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
