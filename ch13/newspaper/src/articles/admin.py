2a92886cfdf5a5a06de366b63e462e647d79f51b
yc.liu <YungChiLiu@NCBM-PCP1911009.local>
2022-01-21 19:37:41 +0800

    update article

---
+++ ch13/newspaper/src/articles/admin.py
@@ -0,0 +1,6 @@
+import imp
+from django.contrib import admin
+
+from .models import Article
+
+admin.site.register(Article)
--- ch13/newspaper/src/newspaper_project/settings.py
+++ ch13/newspaper/src/newspaper_project/settings.py
@@ -32,9 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
+    # 3rd Party
     'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,26 +112,20 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Asia/Taipei'
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-AUTH_USER_MODEL = 'users.CustomUser'  # Use Custom User instead of build-in-User
+# Use Custom User instead of build-in-User
+AUTH_USER_MODEL = 'users.CustomUser'  
 
 # LogIn redirect
 LOGIN_REDIRECT_URL = 'home'
