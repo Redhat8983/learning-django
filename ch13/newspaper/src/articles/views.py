b3d3c0b9c0a9cded40ab42bd51d1aa2514692c2d
yc.liu <yc@example.com>
2022-01-21 20:26:36 +0800

    add New article page

--- ch13/newspaper/src/articles/views.py
+++ ch13/newspaper/src/articles/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from dataclasses import fields
+from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -21,3 +22,8 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ( 'title', 'body', 'author',)
