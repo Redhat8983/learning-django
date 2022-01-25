01f5bdf629807d76bee51ac44d077a896e92ce78
yc.liu <YungChiLiu@NCBM-PCP1911009.local>
2022-01-25 14:14:00 +0800

    inint ch14 Permissions

---
+++ ch14/newspaper/src/articles/views.py
@@ -0,0 +1,29 @@
+from dataclasses import fields
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ( 'title', 'body', 'author',)
